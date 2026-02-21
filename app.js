c6ef14a13ddc4a30404c14f267016f0eecfbece0
NECX12 <ejikechukwuma99@gmail.com>
2026-02-21 13:47:49 +0100

    Updating the middlewares

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+require("dotenv").config()
 const express = require('express');
+const joi = require('joi');
+const logRequest = require("./middleware/logger.js");
+const validateTodo = require("./middleware/validator.js")
+const validateTodoPatch = require("./middleware/validatepatch.js")
+const errorHandler = require("./middleware/errorhandler.js")
+
 const app = express();
+
 app.use(express.json()); // Parse JSON bodies
+app.use(logRequest); // Logging the Request
 
 let todos = [
   { id: 1, task: 'Learn Node.js', completed: false },
@@ -14,20 +23,28 @@ app.get('/todos', (req, res) => {
 
 // GET /todos/active (filter !completed)
 app.get('/todos/active', (req, res) => {
-  const activeTodos = todos.filter(todo => !todo.completed);
+  try {
+    const activeTodos = todos.filter(todo => !todo.completed);
   res.status(200).json(activeTodos);
+  } catch (error) {
+    next(error)
+  }
 });
 
 // Get /todo/:id (single read)
-app.get('/todos/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
+app.get('/todos/:id', (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id, 10);
   const todo = todos.find(e => e.id == id);
   if (todo) {
     res.status(200).json(todo);
   } else {
     res.status(404).json({message: "This todo is nowhere to be found"})
   }
-})
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Validation: POST requires "tast" field
 app.post("/task-checker", (req, res) => {
@@ -51,39 +68,52 @@ app.post("/task-checker", (req, res) => {
 
 
 // POST New – Create
-app.post('/todos', (req, res) => {
-  const newTodo = { id: todos.length + 1, ...req.body }; // Auto-ID
+app.post('/todos', validateTodo, (req, res, next) => {
+  try {
+    const newTodo = { id: todos.length + 1, ...req.body }; // Auto-ID
   todos.push(newTodo);
   res.status(201).json(newTodo); // Echo back
+  } catch (error) {
+    next(error)
+  }
 });
 
 // PATCH Update – Partial
-app.patch('/todos/:id', (req, res) => {
-  const todo = todos.find((t) => t.id === parseInt(req.params.id)); // Array.find()
+app.patch('/todos/:id', validateTodoPatch, (req, res, next) => {
+  try {
+    const todo = todos.find((t) => t.id === parseInt(req.params.id)); // Array.find()
   if (!todo) return res.status(404).json({ message: 'Todo not found' });
   Object.assign(todo, req.body); // Merge: e.g., {completed: true}
   res.status(200).json(todo);
+  } catch (error) {
+    next(error)
+  }
 });
 
 // DELETE Remove
-app.delete('/todos/:id', (req, res) => {
-  const id = parseInt(req.params.id);
+app.delete('/todos/:id', (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id);
   const initialLength = todos.length;
   todos = todos.filter((t) => t.id !== id); // Array.filter() – non-destructive
   if (todos.length === initialLength)
     return res.status(404).json({ error: 'Not found' });
   res.status(204).send(); // Silent success
+  } catch (error) {
+    next(error)
+  }
 });
 
-app.get('/todos/completed', (req, res) => {
-  const completed = todos.filter((t) => t.completed);
+app.get('/todos/completed', (req, res, next) => {
+  try {
+    const completed = todos.filter((t) => t.completed);
   res.json(completed); // Custom Read!
+  } catch (error) {
+    next(error)
+  }
 });
 
+app.use(errorHandler);
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ error: 'Server error!' });
-});
-
-const PORT = 3002;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server on port http://localhost:${PORT}`));
---
+++ middleware/validatepatch.js
@@ -0,0 +1,18 @@
+const Joi = require("joi")
+
+const validateTodoPatch = (req, res, next) => {
+    const schema = Joi.object({
+        task : Joi.string().min(3).max(100),
+        completed : Joi.boolean().default(false)
+    })
+
+    const {error} = schema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({error: error.details[0].message})
+    }
+
+    next();
+};
+
+module.exports = validateTodoPatch;
